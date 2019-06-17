dd994a0d818fb818988a672fcbbd118af9e087b3
chedly <chedly@DESKTOP-B2NOGRQ>
2019-06-17 13:27:27 +0100

    added service

---
+++ src/services/crudProfessionnel.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entites.Service;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import utils.MyDB;
+import interfaces.IcrudProfessionnel;
+
+/**
+ *
+ * @author chedly
+ */
+public class crudProfessionnel implements IcrudProfessionnel{
+   private Statement statement;
+    private Connection con;
+    private ResultSet resultat = null;
+
+    public crudProfessionnel() {
+        try {
+            statement = MyDB.getInstance().getCnx().createStatement();
+            con = MyDB.getInstance().getCnx();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    @Override
+    public void addServices(Service s) {
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO service  (description , nomService , serviceID) VALUES(?,?,null);");
+                    
+        
+            preparedStatement.setString(1, s.getDescription());
+            preparedStatement.setString(2, s.getNameService());
+            preparedStatement.setInt(3, s.getId());
+           
+ 
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        
+        
+    }
+    
+}
